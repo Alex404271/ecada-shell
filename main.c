a8dfc9c6ea04ea52d5bb0e1208da70610703b06b
Alex404271 <ar9835033@gmail.com>
2026-05-17 20:35:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -15,6 +16,35 @@ int main(int argc, char *argv[]) {
       break;
     } else if (strncmp(input, "echo ", 5) == 0) {
       printf("%s\n", input + 5);
+    } else if (strncmp(input, "type ", 5) == 0) {
+      char *arg = input + 5;
+      if (strcmp(arg, "exit") == 0 || strcmp(arg, "echo") == 0 ||
+          strcmp(arg, "type") == 0) {
+        printf("%s is a shell builtin\n", arg);
+      } else {
+        char *path_env = getenv("PATH");
+        if (path_env != NULL) {
+          char path_copy[1000];
+          strncpy(path_copy, path_env, sizeof(path_copy) - 1);
+          path_copy[sizeof(path_copy) - 1] = '\0';
+          char *dir = strtok(path_copy, ":");
+          int found = 0;
+          while (dir != NULL && !found) {
+            char full_path[1100];
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+            if (access(full_path, X_OK) == 0) {
+              printf("%s is %s\n", arg, full_path);
+              found = 1;
+            }
+            dir = strtok(NULL, ":");
+          }
+          if (!found) {
+            printf("%s: not found\n", arg);
+          }
+        } else {
+          printf("%s: not found\n", arg);
+        }
+      }
     } else {
       printf("%s: command not found\n", input);
     }
